0982f88e0c8e5aa0a0ee1b76aada2343ad172eb6
NinNiNanNa <kimsojin3011@gmail.com>
2024-03-28 15:46:49 +0900

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import GlobalStyle from "./GlobalStyle";
+import "./assets/fonts/font.css";
+import { ThemeProvider } from "styled-components";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { theme } from "./theme";
+
+const client = new QueryClient();
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  // <React.StrictMode>
+  <QueryClientProvider client={client}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>
+  // </React.StrictMode>
+);
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  red: "#E51013",
+  black: {
+    darker: "#000000",
+    lighter: "#141414",
+  },
+  white: {
+    darker: "#808080",
+    lighter: "#ffffff",
+  },
+  green: "#46d369",
+};
